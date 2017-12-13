87cf6bbfdd55f6580d2498578b1c5bd13663826a
ChrisHayward <32181353+ChrisHayward@users.noreply.github.com>
2017-12-13 06:09:51 -0800

    Cleaned up comments

--- final.asm
+++ final.asm
@@ -12,20 +12,20 @@ main:
 stringToInt:
 
 		addi $sp, $sp, -20	# move stack pointer
-		sw $ra, 16($sp)		#save $ra to stack
-		sw $s3, 12($sp)		#save $s3 to stack,
-		sw $s2, 8($sp)		#save $s2 to stack
-		sw $s1, 4($sp)		#save $s1 to stack
-		sw $s0, 0($sp)		#save $s0 to stack
+		sw $ra, 16($sp)		#save $ra, $s3, $s2, $s1, and $s0 to stack
+		sw $s3, 12($sp)		
+		sw $s2, 8($sp)		
+		sw $s1, 4($sp)		
+		sw $s0, 0($sp)		
 		
 		li, $v0, 0		#set the return initally to 0
-		li, $s2, 0		#make sure $s2 starts as 0
-		li, $s3, 0		#make sure $s3 starts as 0
+		li, $s2, 0		#make sure $s2 and $s3 start as 0
+		li, $s3, 0		
 checkBase:
 		slti $t0, $a1, 2	#sets $t0 to 1 if the base argument is LESS than 2
 		slti $t1, $a1, 17	#sets $t1 to 1 if the base argument is LESS than 17
 		not $t1, $t1		#bitwise NOT of $t1 so that it gives if the argument is MORE than 16
-		andi $t1, $t1, 1
+		andi $t1, $t1, 1	#removes extraneous 1's created by the previous NOT
 		or $t0, $t1, $t0	#if argument is outside either bound, $t0 should be 1	
 		bne $t0, 0, ErrorBase	#if base is outside accepted range, jump to ErrorBase to handle, else continue
 
@@ -33,15 +33,15 @@ checkNeg:	lbu	$t0, 0($a0)	#load first character of string
 		li	$t1, '-'	#load ascii '-' for negative comparison
 		bne	$t0, $t1, checkCharValue
 		li	$s3, 1		#if string starts with '-' and is therefore negative, set $s3 to 1 for use later
-		addi $a0, $a0, 1	#increment to next char
+		addi $a0, $a0, 1	#increment and load next char
 		lbu	$t0, 0($a0)
 		
 checkCharValue:	
 		beqz $t0, doConversion
-		jal	isDigitOrNot	#jump to check if current character is a digit or a letter(or something else entirely)
+		jal	isDigitOrNot	#jump to check if current character is a digit or a letter(or invalid)
 		addi $s2, $s2, 1	#add 1 to count length of given string
-		addi $a0, $a0, 1	#increment to next character
-		lbu	$t0, 0($a0)	#load next character
+		addi $a0, $a0, 1	#increment and load next character
+		lbu	$t0, 0($a0)	
 		j checkCharValue
 
 isDigitOrNot:		
@@ -52,7 +52,7 @@ isDigitOrNot:
 		
 		sub $t6, $t0, $t1		#gets value of digit
 		slt $t2, $t6, $a1		#if this gives a value of 1, then the digit is valid for the given base
-		beq $t2, $0, ErrorChar
+		beq $t2, $0, ErrorChar		
 		
 		jr	$ra
 isNotDigit:	
@@ -84,26 +84,26 @@ Loop:		slt $t3, $s0, $s2	#beginning test for while loop given in assignment inst
 		j Loop
 				
 endLoop:
-		beq $s3, 0, returnBookKeep	#checks value stored in $s3 earlier for negative or not, if 0, then positive, and jump to next section
+		beq $s3, 0, returnBookKeep	#checks value stored in $s3 earlier for negative or not, if 0, then result should be positive, and jump to next section
 		not $v0, $v0		
 		addi $v0, $v0, 1	#twos compliment conversion
 		
 
-returnBookKeep:	lw $s0, 0($sp)		# restore $s0 from stack
-		lw $s1, 4($sp)		# restore $s1 from stack
-        	lw $s2, 8($sp)		# restore $s2 from stack
-        	lw $s3, 12($sp)		# restore $s3 from stack
-        	lw $ra, 16($sp)		# restore $ra from stack
+returnBookKeep:	lw $s0, 0($sp)		# restore $ra, $s3, $s2, $s1, and $s0 from stack
+		lw $s1, 4($sp)		
+        	lw $s2, 8($sp)		
+        	lw $s3, 12($sp)		
+        	lw $ra, 16($sp)		
         	addi $sp,$sp, 20	# restore stack pointer
         	jr $ra
 ErrorBase:
-		la $a0, ErrorBaseRange
+		la $a0, ErrorBaseRange	#Prints Base Error message
 		li $v0, 4
 		syscall
 		li $v0, 0
 		j returnBookKeep
 ErrorChar:	
-		la $a0, ErrorValidChar
+		la $a0, ErrorValidChar	#Prints Invalid Character Error message
 		li $v0, 4
 		syscall
 		li $v0, 0
